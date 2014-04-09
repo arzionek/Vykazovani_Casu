6accac69f54fdb126f2d0b3d1c3995fb710e5efa
jstrolen <2ferdan@gmail.com>
2014-04-09 23:32:42 +0200

    Změna kódování na UTF-8 pro správný import diakritiky

--- Vykazovani_Casu/WebContent/scripty/inserty.sql
+++ Vykazovani_Casu/WebContent/scripty/inserty.sql
@@ -1,9 +1,9 @@
 insert into uzivatel(uzivatel_id, jmeno, prijmeni, titul_pred, titul_za, login, heslo) VALUES
-(1, 'VendelÌn', 'Pokus', 'prof. Ing.', 'Ph.D.', 'admin', 'admin'),
-(2, 'Marek', 'Vohradsk˝', 'Bc.', '', 'capo333', 'xxx'),
-(3, 'VÌt', 'ätÏp·nek', 'Bc.', '', 'stepanev', 'xxx'),
-(4, 'Josef', 'Strolen˝', '', '', 'jstrolen', 'xxx'),
+(1, 'Vendel√≠n', 'Pokus', 'prof. Ing.', 'Ph.D.', 'admin', 'admin'),
+(2, 'Marek', 'Vohradsk√Ω', 'Bc.', '', 'capo333', 'xxx'),
+(3, 'V√≠t', '≈†tƒõp√°nek', 'Bc.', '', 'stepanev', 'xxx'),
+(4, 'Josef', 'Strolen√Ω', '', '', 'jstrolen', 'xxx'),
 (5, 'Jaroslav', 'Ullmann', '', '', 'ullmannj', 'xxx'),
-(6, 'Roman', 'MouËek', 'Ing.', 'Ph.D.', 'moucek', 'xxx'),
-(7, 'Petr', 'Jeûek', 'Ing.', 'Ph.D.', 'jezekp', 'xxx');
+(6, 'Roman', 'Mouƒçek', 'Ing.', 'Ph.D.', 'moucek', 'xxx'),
+(7, 'Petr', 'Je≈æek', 'Ing.', 'Ph.D.', 'jezekp', 'xxx');
 
