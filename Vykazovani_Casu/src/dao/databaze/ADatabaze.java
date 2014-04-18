3084a6e90339deed441c771a8a4763d9096c5296
Bc. Vít Štěpánek <v.stepanek@centrum.cz>
2014-04-18 11:07:32 +0200

    zkouška ošetření výjimky vypršení sezení

--- Vykazovani_Casu/src/dao/databaze/ADatabaze.java
+++ Vykazovani_Casu/src/dao/databaze/ADatabaze.java
@@ -1,5 +1,6 @@
 package dao.databaze;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
@@ -7,9 +8,13 @@ import org.hibernate.Hibernate;
 import org.hibernate.Query;
 import org.hibernate.Session;
 
+import dao.model.NastaveniSystemu;
 import dao.util.HibernateHelper;
 
 public abstract class ADatabaze{
+  
+  private static final String POSLEDNI_PRIPOJENI = "posledni.pripojeni";
+  private boolean kontrola = true;
   
   protected Session ctx;
   
@@ -18,11 +23,13 @@ public abstract class ADatabaze{
   }
   
   public Object nacti(Class<?> trida, Long id) {
+    zkontrolujSession();
     Object object = ctx.load(trida, id);
     return object;
   }
   
   public Object nacti(Class<?> trida, String atribut, String hodnota){
+    if(kontrola) zkontrolujSession();
     Query query = ctx.createQuery("select o from " + trida.getName() + " o where o." + atribut + "='" + hodnota + "'");
     List<?> list = query.list();
     if(list != null && !list.isEmpty()) return list.get(0);
@@ -30,6 +37,7 @@ public abstract class ADatabaze{
   }
   
   public Object nacti(Class<?> trida, Object atributy[], Object hodnoty[]){
+    zkontrolujSession();
     Query query = ctx.createQuery(vytvorDotaz(trida, atributy, hodnoty));
     List<?> list = query.list();
     if(list != null && !list.isEmpty()) return list.get(0);
@@ -37,6 +45,7 @@ public abstract class ADatabaze{
   }
   
   public void vlozUprav(Object object, Long id) {
+    if(kontrola) zkontrolujSession();
     if(id == null){
       ctx.beginTransaction();
       ctx.save(object);
@@ -49,12 +58,14 @@ public abstract class ADatabaze{
   }
   
   public void smaz(Object object) {
+    zkontrolujSession();
     ctx.beginTransaction();
     ctx.delete(object);
     ctx.getTransaction().commit();
   }
   
   public List<?> ziskejObjekty(Class<?> trida, Object[] atributy, Object[] hodnoty) {
+    zkontrolujSession();
     Query query = ctx.createQuery(vytvorDotaz(trida, atributy, hodnoty));
     List<?> list = query.list();
     return list;
@@ -71,6 +82,23 @@ public abstract class ADatabaze{
   }
 
   public void inicializaceSetu(Set<?> set) {
+    zkontrolujSession();
     Hibernate.initialize(set);
+  }
+
+  private void zkontrolujSession() {
+    try{
+      kontrola = false;
+      NastaveniSystemu nastaveni = (NastaveniSystemu) nacti(NastaveniSystemu.class, "nazev", POSLEDNI_PRIPOJENI);
+      if(nastaveni == null) nastaveni = new NastaveniSystemu();
+      nastaveni.setNazev(POSLEDNI_PRIPOJENI);
+      nastaveni.setHodnota((new Date()).toString());
+      vlozUprav(nastaveni, nastaveni.getId());
+      kontrola = true;
+    }catch(Throwable th){
+      System.out.println(new Date() + " " + th.getMessage());
+      HibernateHelper.getInstance().closeSession();
+      ctx = HibernateHelper.getInstance().getSession();
+    }
   }
 }
