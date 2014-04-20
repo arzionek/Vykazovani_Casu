5eb90d623395359e3f3c87f4b9c00940965bde04
Bc. Vít Štěpánek <v.stepanek@centrum.cz>
2014-04-20 19:55:22 +0200

    upraveny knihovny a metoda pro získání informací o sloupci

--- Vykazovani_Casu_Model/src/dao/model/AEntita.java
+++ Vykazovani_Casu_Model/src/dao/model/AEntita.java
@@ -2,8 +2,11 @@ package dao.model;
 
 import java.util.Iterator;
 
+import org.hibernate.cfg.Configuration;
 import org.hibernate.mapping.Column;
 import org.hibernate.mapping.Property;
+
+import dao.util.HibernateHelper;
 
 public abstract class AEntita implements java.io.Serializable {
 
@@ -12,11 +15,15 @@ public abstract class AEntita implements java.io.Serializable {
    */
   private static final long serialVersionUID = 258378465050075685L;
 
+  private static final Configuration cfg = HibernateHelper.getInstance().getConfiguration();
+  private static boolean vytvoreno = false;
+  
   public static Column getOmezeniSloupce(Class<?> trida, String atribut){
-    Property p = (new org.hibernate.cfg.Configuration())
-      .addClass(trida)
-      .getClassMapping(trida.getName())
-      .getProperty(atribut);
+    if(!vytvoreno){
+      cfg.buildMappings();
+      vytvoreno = true;
+    }
+    Property p = cfg.getClassMapping(trida.getName()).getProperty(atribut);
     Iterator<?> iter = p.getColumnIterator();
     Column c = null;
     if(iter.hasNext()) c = (Column) iter.next();
