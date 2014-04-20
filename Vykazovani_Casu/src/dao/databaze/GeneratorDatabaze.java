4d5977fe199fd769e33f6b5d3358879d7b3ec42d
Bc. Vít Štěpánek <v.stepanek@centrum.cz>
2014-04-20 17:00:22 +0200

    přidána podpora utf-8

--- Vykazovani_Casu/src/dao/databaze/GeneratorDatabaze.java
+++ Vykazovani_Casu/src/dao/databaze/GeneratorDatabaze.java
@@ -36,8 +36,20 @@ public class GeneratorDatabaze {
         index = dokument.indexOf("drop foreign key " + klic + ";");
       }
       
+      String dokument2 = dokument;
+      String pridat = " ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci";
+      index = dokument2.indexOf("create table");
+      int index2 = dokument2.indexOf("add index");
+      int posunuti = 0;
+      for (int j = index; j < index2; j++) {
+        if(dokument.charAt(j) == ';'){
+          dokument2 = dokument2.substring(0, posunuti + j) + pridat + dokument2.substring(posunuti + j);
+          posunuti += pridat.length();
+        }
+      }
+      
       FileWriter fw = new FileWriter(nazev);
-      fw.write(dokument);
+      fw.write(dokument2);
       fw.close();
       //hibernate.main(new String[]{});
     } catch (Exception e) {
